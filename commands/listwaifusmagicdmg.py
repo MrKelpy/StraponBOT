f7ee0c2453b7819a0d55463ce8df3661f63c8e83
MrKelpy <alexandresilva.coding@gmail.com>
2022-04-28 10:46:07 +0100

    Added loads of listing commands. Added peek!

---
+++ commands/listwaifusmagicdmg.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+This file is distributed as part of the StraponBOT Project.
+The source code may be available at
+https://github.com/MrKelpy/StraponBOT
+
+If a license applies for this project, the former can be found
+in every distribution, as a "LICENSE" file at top level.
+"""
+
+# Built-in Imports
+from typing import List
+
+# Third Party Imports
+import discord
+from discord.ext import commands
+
+# Local Application Imports
+from data.globals import bot
+from resources.waifu_utils.do_prelisting_jobs import do_prelisting_jobs
+from resources.LaminariaDB.Document import Document
+from tasks.handle_waifu_listing import handle_waifu_listing
+from resources.waifu_dict_loaders.listwaifus_dict_loader import make_dict_loader as lw_dict_loader
+
+
+@bot.command(description="|LISTING| Lists all of the waifus in the database for a given user based on their magical damage",
+             aliases=("lwmdmg",))
+async def listwaifusmagicdmg(ctx: commands.Context, starting_index: int = 1):
+    """
+    Lists all the waifus in the database for a given user, in a paged list of 15 waifus per, based
+    on their magical damage.
+
+    :param commands.Context ctx: The command context
+    :param int starting_index: The starting index for the paged list.
+    :return None:
+    """
+
+    waifu_query, listing_message = await do_prelisting_jobs(ctx, starting_index)
+    if not waifu_query: return
+    if starting_index < 0: starting_index = 1
+
+    waifu_list: List[Document] = sorted(waifu_query, key=lambda x: x.content["magical_dmg"], reverse=True)
+    cache_msg: discord.Message = discord.utils.get(bot.cached_messages, id=listing_message.id)
+    bot.loop.create_task(handle_waifu_listing(ctx, waifu_list, cache_msg, starting_index, lw_dict_loader))
---
+++ commands/peek.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+This file is distributed as part of the StraponBOT Project.
+The source code may be available at
+https://github.com/MrKelpy/StraponBOT
+
+If a license applies for this project, the former can be found
+in every distribution, as a "LICENSE" file at top level.
+"""
+
+# Built-in Imports
+from typing import List
+
+# Third Party Imports
+from discord.ext import commands
+import discord
+
+# Local Application Imports
+from data.globals import bot, FAILED_EMOJI
+from resources.LaminariaCore.utils.discordpy import DPyUtils
+from resources.ensure_collection import ensure_collection
+from resources.LaminariaDB.Collection import Collection
+from resources.LaminariaDB.Document import Document
+from tasks.handle_waifu_listing import handle_waifu_listing
+from resources.waifu_dict_loaders.listwaifusimage_dict_loader import make_dict_loader as lwi_dict_loader
+
+
+@bot.command(description="Shows information about all the characters matching the name given")
+async def peek(ctx: commands.Context, *, waifu_name: str):
+    """
+    Shows panelled informatiom about a waifu from anyone in the database that matches the name given.
+
+    :param commands.Context ctx: The command context
+    :param str waifu_name: The waifu to show the information for
+    :return None:
+    """
+
+    # Send a loading message to the channel, in order to create the message that will be checked by the task.
+    dpy_utils: DPyUtils = DPyUtils()
+    listing_message: discord.Message = await dpy_utils.send_loading(ctx.channel)
+
+    waifu_collection: Collection = await ensure_collection(str(ctx.guild.id))
+    waifu_query: List[Document] = waifu_collection.find(query={"name": waifu_name})
+
+    if not waifu_query:
+        await listing_message.edit(content="No results!", embed=None)
+        await ctx.message.add_reaction(FAILED_EMOJI)
+        return
+
+    waifu_list: List[Document] = sorted(waifu_query, key=lambda x: x.content["name"])
+    cache_msg: discord.Message = discord.utils.get(bot.cached_messages, id=listing_message.id)
+    bot.loop.create_task(handle_waifu_listing(ctx, waifu_list, cache_msg, 1, lwi_dict_loader))
